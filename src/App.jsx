bd63b917a81e31a97258bc3165769b4929a88fcb
zalesiu <zalewmat31@gmail.com>
2023-08-30 18:38:53 +0200

    add fetchData funcionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useEffect, useState } from "react";
+
+const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tabs, setTabs] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Error");
+      }
+      const data = await response.json();
+      setTabs(data);
+    } catch (err) {
+      console.log(err);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
   return <h2>Tabs Starter</h2>;
 };
 export default App;
